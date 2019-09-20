9a959df53a3174851263a67ecb24d1a69b20d05d
karrikas <karrikas@gmail.com>
2019-09-20 14:26:13 +0200

    kontaktua eguneratua

--- breakout/assets/js/main.js
+++ breakout/assets/js/main.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    $("#kontaktua").html(
-        " +34 652 73 51 48"+ 
-        ' | '+
-        ' <a href="mailto:karrikas@karrikas.com">k@karrikas.com</a>'
-    );
+    $("#kontaktua").html('<a href="tel:+34 652 73 51 48">652 73 51 48</a>');
 });
